0e9470feb823755f0a2a3c176aad1d13898bc032
mertuess <folsonner@yandex.ru>
2026-03-25 15:11:11 +0300

    Добавил DataManager.cs

---
+++ src/DataManager.cs
@@ -0,0 +1,11 @@
+﻿namespace UniSchedule
+{
+    static public class DataManager
+    {
+        static public List<UniSchedule.Models.Facult> Facultes;
+        static public List<UniSchedule.Models.Course> Courses;
+        static public List<UniSchedule.Models.Teacher> Teachers;
+        static public List<UniSchedule.Models.Group> Groups;
+        static public List<string> CurrentDates;
+    }
+}
